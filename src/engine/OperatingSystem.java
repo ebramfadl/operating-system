ddf0a4e24f350c9374bf525ae407957554a79edd
ebramfadl <ebram@ntsal.com>
2023-05-16 22:27:25 +0300

    kick off

---
+++ src/engine/OperatingSystem.java
@@ -0,0 +1,129 @@
+package engine;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class OperatingSystem {
+
+    //<p1,p2,p3>
+    // 0  1  2
+    private ArrayList<Object[]> memory;
+    private int availableMemorySpace;
+    private int numberOfProcesses;
+    private Queue<Integer> readyQueue;
+    private Queue<Integer> blockedQueue;
+
+    private boolean fileMutex;
+    private boolean inputMutex;
+    private boolean outputMutex;
+
+    public OperatingSystem(){
+        memory = new ArrayList<>();
+        availableMemorySpace = 40;
+        numberOfProcesses = 0;
+        readyQueue = new LinkedList<>();
+        blockedQueue = new LinkedList<>();
+    }
+
+    public void createProcess(String filePath){
+
+        File file = new File(filePath);
+        ArrayList<String> instructions = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()){
+                String line = scanner.nextLine();
+                instructions.add(line);
+            }
+            scanner.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Object[] arr = new Object[8+instructions.size()];
+        availableMemorySpace -= arr.length;
+
+        arr[0] = null;//a
+        arr[1] = null;//b
+        arr[2] = null;//c
+        arr[3] = ++numberOfProcesses;
+        arr[4] = 8;
+        arr[5] = ProcessState.CREATED;
+        arr[6] = 8;
+        arr[7] = 7+instructions.size();
+
+        int j = 8;
+        for ( int i = 0 ; i < instructions.size() ; i++){
+            arr[j] = instructions.get(i);
+            j++;
+        }
+
+        memory.add(arr);
+        readyQueue.add((int)arr[3]);
+
+    }
+
+    public void displayMemoryContent(){
+
+        for (Object[] process : memory){
+            System.out.println("======================================Process "+process[3]+"====================================");
+            System.out.println("0 : a = "+process[0]);
+            System.out.println("1 : b = "+process[1]);
+            System.out.println("2 : c = "+process[2]);
+            System.out.println("3 : ID = "+process[3]);
+            System.out.println("4 : PC = "+process[4]);
+            System.out.println("5 : State = "+process[5]);
+            System.out.println("6 : Instruction Start = "+process[6]);
+            System.out.println("7 : Instruction End = "+process[7]);
+
+            for (int i = 8 ; i < process.length ; i++){
+                if ((int)process[4] == i){
+                    System.out.println(i + " : instruction = "+process[i] + "     <<<<<<<<<<< PC");
+                }
+                System.out.println(i + " : instruction = "+process[i]);
+            }
+            System.out.println("=====================================================================");
+            System.out.println();
+
+        }
+
+    }
+
+    public String toString(){
+        String str = "";
+        str += "Number of processes = "+numberOfProcesses+"\n"+
+        "Available Memory Space = "+availableMemorySpace+"\n"+
+        "Ready Queue [ ";
+
+        for (Integer pid : readyQueue){
+            str += pid + " , ";
+        }
+        str += "]"+"\n";
+
+        str += "Blocked Queue [ ";
+        for (Integer pid : blockedQueue){
+            str += pid + " , ";
+        }
+        str += "]"+"\n";
+
+        return str;
+    }
+
+    public static void main(String[] args) {
+
+        OperatingSystem os = new OperatingSystem();
+//        os.createProcess("src/Program_1.txt");
+
+        for (int i = 1 ; i <= 3 ; i++){
+            os.createProcess("src/Program_"+i+".txt");
+        }
+        System.out.println(os);
+        os.displayMemoryContent();
+    }
+
+}
---
+++ src/engine/PCB.java
@@ -0,0 +1,60 @@
+package engine;
+
+public class PCB {
+
+    private int processID;
+    private ProcessState state;
+    private int PC;
+    private int start;
+    private int end;
+
+    public PCB(int processID, ProcessState state, int PC, int start, int end) {
+        this.processID = processID;
+        this.state = state;
+        this.PC = PC;
+        this.start = start;
+        this.end = end;
+    }
+
+
+    public int getProcessID() {
+        return processID;
+    }
+
+    public void setProcessID(int processID) {
+        this.processID = processID;
+    }
+
+    public ProcessState getState() {
+        return state;
+    }
+
+    public void setState(ProcessState state) {
+        this.state = state;
+    }
+
+    public int getPC() {
+        return PC;
+    }
+
+    public void setPC(int PC) {
+        this.PC = PC;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+}
